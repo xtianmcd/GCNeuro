d42a22017e488eca22ad7e263d8aa89bc7ecc60b
xtianmcd <clm121@uga.edu>
2019-04-25 16:54:22 -0400

    preproc_stage2 files

--- bcg.py
+++ bcg.py
@@ -3,6 +3,10 @@ import os
 import subprocess
 import nibabel as nib
 import numpy as np
+import json
+# from dipy.tracking.streamline import transform_streamlines
+from dipy.tracking import utils
+from scipy.io import loadmat
 
 
 def bash_command(string_cmd):
@@ -10,33 +14,88 @@ def bash_command(string_cmd):
     output, error = process.communicate()
     return output
 
+def gen_hshtbl(trk):
+    hshtbl = []
+    for sl in trk:
+        hshtbl.append(sum([256**2*c[0]+256*c[1]+c[2] for c in sl]))
+    if len(trk) != len(np.unique(np.array(hshtbl))): print("Streamline hashing generated equivalent hashes")
+    return hshtbl
+
+
+def gen_masks(fs_dir,subrun):
+    parcseg = os.path.join(fs_dir,subrun,'mri','aparc+aseg.mgz')
+    anat = nib.load(parcseg)
+    anat_img = anat.get_fdata().astype(int)
+    # anat_aff = anat.affine
+    mask_dir = os.path.join(fs_dir,subrun,'seg_masks')
+    bash_command(f'mkdir {mask_dir}')
+    for label in np.unique(anat_img):
+        mask = np.ma.array(anat_img, mask = anat_img != label, fill_value=0).filled()
+        np.save(os.path.join(mask_dir,f'{label}_msk'), mask)
+    return mask_dir,parcseg
+
 def read_trkfile(run):
-
+    extra_calc=False
     trkfile = nib.streamlines.load(run)
-    tracks = np.asarray(trkfile.tractogram.streamlines)
-    return tracks
+    trk_img = trkfile.tractogram.streamlines
+    trk_aff = trkfile.affine
+    ht = gen_hshtbl(trk_img)
+    return trk_img,trk_aff,ht
 
-def get_trks(subject):
-    streamlines={}
+def get_trks(subject, maindir,dtk_path):
     if subject.split('-')[0]=='sub':
         sub = maindir+subject
-        trkdir = sub+'/dwi/tracks'
+        trkdir = os.path.join(sub,'dwi','tracks')
+        fsdir = os.path.join(sub,'anat','freesurfer')
         for run in os.listdir(trkdir):
-            # streamlines[f'{run}']={}
-            run_algos={}
-            trkrun = trkdir+run
-            for algo in os.listdir(trkrun):
-                # algo=trkrun+algo_file
-                if algo.endswith('.trk'): run_algo[f'{algo}'] = read_trks(f'{trkrun+algo}')
-            streamlines[f'{run}'] = run_algos
-    return streamlines
+            trkrun = os.path.join(trkdir,run)
+            # run_algos={}
+            if os.path.isdir(trkrun) and run in os.listdir(fsdir):
+                print(run)
+                maskdir,mgz = gen_masks(fsdir,run)
+                nii = mgz.split('.')[0]+'.nii'
+                bash_command(f'mri_convert --in_type mgz --out_type nii {mgz} {nii}')
+                bash_command(f'flirt -in {os.path.join(sub,"dwi","dtk",run)}_dwi.nii -ref {nii} -omat {os.path.join(sub,"dwi",run)}_reg.mtx -out {os.path.join(sub,"dwi",run)}_reg')
+                algo_vxl_sl={}
+                for algo in os.listdir(trkrun):
+                    if algo.endswith('.trk') and 'fltr' in algo and 'reg' not in algo:
+                        print(algo)
+                        bash_command(f'{dtk_path}track_transform {os.path.join(trkrun,algo)} {os.path.join(trkrun,algo.split(".")[0])}_reg.trk -src {os.path.join(sub,"dwi","dtk",run)}_dwi.nii -ref {nii} -reg {os.path.join(sub,"dwi",run)}_reg.mtx -reg_type flirt')
+                        algo_trks,algo_aff,hshtbl = read_trkfile(f'{os.path.join(trkrun,algo.split(".")[0])}_reg.trk')
+                        # with open(f'{os.path.join(trkrun,algo.split(".")[0])}_sl.txt','w') as ht:
+                        #     ht.write(hshtbl)
+                        np.savetxt(f'{os.path.join(trkrun,algo.split(".")[0])}_sl.txt',np.array(hshtbl))
+                        vxl_sl={}
+                        for maskf in os.listdir(maskdir):
+                            if maskf.endswith('npy'):
+                                print(maskf)
+                                mask = np.load(os.path.join(maskdir,maskf))
+                                vxl_sl[maskf.split("_")[0]]=gen_hshtbl(list(utils.target(algo_trks,mask,algo_aff)))
+
+                        sl_vxl={}
+                        ks = sorted([int(k) for k in vxl_sl.keys()])
+                        for kE in ks:
+                            sl_vxl[str(kE)]=vxl_sl[str(kE)]
+                        algo_vxl_sl[algo.split('.')[0]]=sl_vxl
+                        # run_algos[f'{("_").join(algo.split(".")[0].split("_")[2:])}'] = [vxl.tolist() for vxl in algo_trks]
+                with open(os.path.join(trkrun,"algo_vol-trk_map.json"),'w') as t:
+                    json.dump(algo_vxl_sl,t)
+    return
 
 
 if __name__=="__main__":
 
-    main_dir = '/Volumes/ElementsExternal/test2/'
+    main_dir = '/Volumes/ElementsExternal/mridti_test2/'
+    dtk_home='/Applications/Diffusion_Toolkit.app/Contents/MacOS/'
 
-    tracks={}
+    # tracks={}
 
     for subdir in os.listdir(main_dir):
-        tracks[f'{subdir}'] = get_trks(subdir)
+        if os.path.isdir(main_dir+subdir):
+            # tracks[f'{subdir}'] = get_trks(subdir,main_dir)
+            get_trks(subdir,main_dir,dtk_home)
+
+    # for k in tracks.keys():
+    #     print(k)
+    # np.save(main_dir+'tracks.npy',tracks)
+    # print(tracks['sub-01']['sub-01_run-01']['sub-01_run-01_fact.trk'])
--- bgg.py
+++ bgg.py
@@ -6,13 +6,13 @@ import numpy as np
 import random
 import json
 import pickle
+import nibabel as nib
 
 
 def bash_command(string_cmd):
     process = subprocess.Popen(string_cmd.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     return output
-
 
 def nearest_voxel(center, roi):
     """ Finds the voxel nearest to the XYZ mean of an ROI that is also part of the ROI """
@@ -25,57 +25,53 @@ def nearest_voxel(center, roi):
             nearest=[vxl]
         elif dist==min_dist:
             nearest.append(vxl)
+    # print(nearest)
     return nearest[random.randint(0,len(nearest)-1)]
 
+def gen_cntrs(main_dir,fs_run):
+    parcseg = os.path.join(main_dir,fs_run,'mri','aparc+aseg.mgz')
+    anat = nib.load(parcseg)
+    anat_img = anat.get_fdata().astype(int)
+    # anat_aff = anat.affine
+    centers={}
+    for label in np.unique(anat_img):
+        mask = np.ma.array(anat_img, mask = anat_img != label, fill_value=0).filled()
+        # print(np.unique(mask))
+        idx = np.array([[a,b,c] for a in range(mask.shape[0]) for b in range(mask.shape[1]) for c in range(mask.shape[2]) if mask[a,b,c]!=0])
+        # print(idx)
+        if len(idx)>0:
+            cntr_vox = np.mean(idx,axis=0)
+            # print(cntr_vox)
+            if cntr_vox not in idx: cntr_vox=nearest_voxel(cntr_vox,idx)
+            centers[str(label)]=cntr_vox
+    print(centers.keys())
+    # cntrs={}
+    # ks = sorted([int(k) for k in centers.keys()])
+    # for kE in ks:
+    #     cntrs[str(kE)]=centers[str(kE)]
+    return centers
 
 def get_rois(sub, maindir):
-    voxs = {}
+    center_vxls={}
     if sub.split('-')[0]=='sub':
         sub_fs = os.path.join(sub,'anat/freesurfer/')
         for run in os.listdir(os.path.join(maindir,sub_fs)):
             if 'sub' in run.split('_')[0]:
                 subrun_fs = os.path.join(sub_fs,run)
-                for hemi in ['rh','lh']:
-                    annot_cmd = f"mri_annotation2label --subject {subrun_fs} --hemi {hemi} --outdir {os.path.join(maindir,subrun_fs,'annot')} --sd {maindir}"
-                    bash_command(annot_cmd)
-                for coord_file in os.listdir(os.path.join(maindir,subrun_fs,'annot')):
-                    with open(f'{os.path.join(main_dir,subrun_fs,"annot",coord_file)}','r') as f:
-                        f.readline()
-                        voxel_num = f.readline()
-                    voxel_info = pd.read_csv(f'{os.path.join(maindir,subrun_fs,"annot",coord_file)}',sep='  ',skiprows=2,usecols=[1,2,3],names=['x','y','z'])#names=['row','x','y','z','unknown'])#,usecols=['x','y','z'])
-                    voxel_info[['z','unknown']] = voxel_info['z'].str.split(' ',expand=True)
-                    voxel_info= voxel_info.drop('unknown',axis=1)
-                    voxel_info['z']=voxel_info['z'].astype(float)
-                    # print(voxel_info.head())
-                    cntr_vox = voxel_info.mean(axis=0).values
-                    # print(cntr_vox)
-                    if cntr_vox in voxel_info.values: print('yes')
-                    else: cntr_vox=nearest_voxel(cntr_vox,voxel_info.values)
-                    # print(cntr_vox)
-                    # print()
-                    voxs[voxel_num.strip()]=cntr_vox
-    s_voxs={}
-    srtd_v=[]
-    # print(voxs.keys())
-    kEz=sorted([int(k) for k in voxs.keys()])
-    # print(kEz)
-    for k in kEz:
-        s_voxs[str(k)] = list(voxs[str(k)])
-        srtd_v.append(voxs[str(k)])
-    return s_voxs, np.array(srtd_v)
+                if os.path.isdir(os.path.join(maindir,subrun_fs)):
+                    center_vxls[run] = gen_cntrs(maindir,subrun_fs)
+
+    return center_vxls
 
 
 if __name__=="__main__":
 
-    main_dir='/Volumes/ElementsExternal/test2'
-    rois_d={}
-    rois_l=[]
+    main_dir='/Volumes/ElementsExternal/mridti_test2'
+
+    subj_centers={}
     for subdir in os.listdir(main_dir):
         if os.path.isdir(os.path.join(main_dir,subdir)):
-            sub_d,sub_l = get_rois(subdir, main_dir)
-            rois_d[subdir]=sub_d
-            rois_l.append(sub_l)
-    with open(f'{os.path.join(main_dir,subdir,"anat","freesurfer","rois.json")}','w') as rois:
-        json.dump(rois_d,rois)
-    with open(f'{os.path.join(main_dir,subdir,"anat","freesurfer","rois.pkl")}','w') as rois:
-        pickle.dump(np.arrays(rois_l),rois)
+            subj_centers[subdir] = get_rois(subdir, main_dir)
+
+    with open(f'{os.path.join(main_dir,"center_vxls.json")}','w') as rois:
+        json.dump(subj_centers,rois)
