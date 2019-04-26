e8b720a0119e831506cd661b339c395e67adda67
xtianmcd <clm121@uga.edu>
2019-04-25 23:56:45 -0400

    reformated and added commenting to earlier files

--- make_bids.py
+++ make_bids.py
@@ -3,80 +3,104 @@ import sys
 import json
 import subprocess
 
-main_dir = "./" + sys.argv[1] + "/"
-subj_dirs = os.listdir(main_dir)
+"""
+SECOND FILE IN PIPELINE (following neuro_format.py)
 
-jsondict = {
-    "Name":sys.argv[1],
-    "BIDSVersion": "1.0.0rc1"
-}
+Changes file structure from to be compatible with BIDS format;
+image sources other than PPMI will probably need changes;
+BIDS is the expected format for libraries such as dcm2niix and is quite readable
 
-with open(main_dir+'dataset_description.json', 'w') as outfile:
-    json.dump(jsondict, outfile)
+To run, requires argument:
 
-srcdata = main_dir+'sourcedata/'
-if not os.path.exists(srcdata):
-    os.mkdir(srcdata)
-derivs = main_dir+'derivatives/'
-if not os.path.exists(derivs):
-    os.mkdir(derivs)
+    `python make_bids.py <name_of_main_directory_with_images>`
 
-sub=0
-for subj in subj_dirs:
-    subject = main_dir+subj
-    if os.path.isdir(subject):
-        run=0
-        sub+=1
-        if sub<=9: sub_name = "sub-0{}".format(sub)
-        else: sub_name = "sub-{}".format(sub)
-        print("Old ID: {} --> NewID: {}".format(subj, sub_name))
-        if not os.path.exists(main_dir+sub_name):
-            os.mkdir(main_dir+sub_name)
-            os.mkdir(main_dir+sub_name+'/anat')
-            os.mkdir(main_dir+sub_name+'/dwi')
+Next file to run is `automate_preproc.py`
+"""
 
-        non_nii = {
-            "anat": list([os.path.join(roota,filenamea) for roota,dirnamesa,filenamesa in os.walk(subject) if len(roota.split('/'))==6 and 'T1' in roota.split('/')[3] for filenamea in filenamesa if not filenamea.endswith('.nii')]),
-            "fa"  : list(os.path.join(rootf,filenamef) for rootf,dirnamesf,filenamesf in os.walk(subject) if len(rootf.split('/'))==6 and 'FA' in rootf.split('/')[3] for filenamef in filenamesf if not filenamef.endswith('.nii'))
-        }
-        if non_nii["anat"]: print("WARNING: some T1 file(s) not .nii format in {} - {}".format(subj, non_nii['anat']))
-        if non_nii["fa"]: print("WARNING: some FA file(s) not .nii format in {} - {}".format(subj, non_nii['fa']))
+def re_format(maindir):
+    main_dir = "./" + maindir + "/"
+    subj_dirs = os.listdir(main_dir)
 
-        anats = list([os.path.join(roota,filenamea) for roota,dirnamesa,filenamesa in os.walk(subject) if len(roota.split('/'))==6 and 'T1' in roota.split('/')[3] for filenamea in filenamesa if filenamea.endswith('.nii')])
-        rawdiffs = list(rootr for rootr,dirnamesr,filenamesr in os.walk(subject) if len(rootr.split('/'))==6 and 'DTI' in rootr.split('/')[3])
-        fadiffs = list(os.path.join(rootf,filenamef) for rootf,dirnamesf,filenamesf in os.walk(subject) if len(rootf.split('/'))==6 and 'FA' in rootf.split('/')[3] for filenamef in filenamesf if filenamef.endswith('.nii'))
-        for anat in anats:
-            run +=1
-            newfn = sub_name+'_run-0{}_T1w.nii'.format(run)
-            anatroot = '/'.join(anat.split('/')[:-1])
-            os.rename(anat, os.path.join(anatroot,newfn))
-            os.rename(os.path.join(anatroot, newfn), os.path.join(main_dir,sub_name,'anat',newfn))
-        run=0
-        for rawdiff in rawdiffs:
-            run+=1
-            newfn = sub_name+'_run-0{}_dwi'.format(run)
-            newdir = os.path.join(main_dir,sub_name,'dwi/')
-            cmd = "dcm2niix -b y -o {} -f {} {}".format(newdir,newfn,rawdiff)
-            process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-            output, error = process.communicate()
-            if not os.path.exists(srcdata+sub_name):
-                os.mkdir(srcdata+sub_name)
-            os.rename(rawdiff,srcdata+sub_name+'/rawdcm_run-0{}/'.format(run))
-        run=0
-        for fadiff in fadiffs:
-            run+=1
-            if not os.path.exists(derivs+sub_name):
-                os.mkdir(derivs+sub_name)
-                os.mkdir(derivs+sub_name+'/fa/')
-            newfn = sub_name+'_run-0{}_dwi.nii'.format(run)
-            faroot = '/'.join(fadiff.split('/')[:-1])
-            os.rename(fadiff, os.path.join(faroot,newfn))
-            os.rename(os.path.join(faroot, newfn), os.path.join(derivs,sub_name,'fa',newfn))
+    jsondict = {
+        "Name":maindir,
+        "BIDSVersion": "1.0.0rc1"
+    }
 
-while len(os.listdir(main_dir)) > sub+4:
-    for root, dirnames, filenames in os.walk(main_dir):
-        if os.path.isfile(os.path.join(root,'.DS_Store')):
-            os.remove(os.path.join(root,'.DS_Store'))
-        if os.path.isdir(root):
-            if not os.listdir(root):
-                os.rmdir(root)
+    with open(main_dir+'dataset_description.json', 'w') as outfile:
+        json.dump(jsondict, outfile)
+
+    srcdata = main_dir+'sourcedata/'
+    if not os.path.exists(srcdata):
+        os.mkdir(srcdata)
+    derivs = main_dir+'derivatives/'
+    if not os.path.exists(derivs):
+        os.mkdir(derivs)
+
+    sub=0
+    for subj in subj_dirs:
+        print(f"File structure for {subj}")
+        subject = main_dir+subj
+        if os.path.isdir(subject):
+            run=0
+            sub+=1
+            if sub<=9: sub_name = "sub-0{}".format(sub)
+            else: sub_name = "sub-{}".format(sub)
+            print("\tOld ID: {} --> NewID: {}\n".format(subj, sub_name))
+            with open(os.path.join(main_dir,'ID_mapping.txt')) as ids:
+                ids.write("Old ID: {} --> NewID: {}\n".format(subj, sub_name))
+            if not os.path.exists(main_dir+sub_name):
+                os.mkdir(main_dir+sub_name)
+                os.mkdir(main_dir+sub_name+'/anat')
+                os.mkdir(main_dir+sub_name+'/dwi')
+
+            non_nii = {
+                "anat": list([os.path.join(roota,filenamea) for roota,dirnamesa,filenamesa in os.walk(subject) if len(roota.split('/'))==6 and 'T1' in roota.split('/')[3] for filenamea in filenamesa if not filenamea.endswith('.nii')]),
+                "fa"  : list(os.path.join(rootf,filenamef) for rootf,dirnamesf,filenamesf in os.walk(subject) if len(rootf.split('/'))==6 and 'FA' in rootf.split('/')[3] for filenamef in filenamesf if not filenamef.endswith('.nii'))
+            }
+            if non_nii["anat"]: print("WARNING: some T1 file(s) not .nii format in {} - {}".format(subj, non_nii['anat']))
+            if non_nii["fa"]: print("WARNING: some FA file(s) not .nii format in {} - {}".format(subj, non_nii['fa']))
+
+            anats = list([os.path.join(roota,filenamea) for roota,dirnamesa,filenamesa in os.walk(subject) if len(roota.split('/'))==6 and 'T1' in roota.split('/')[3] for filenamea in filenamesa if filenamea.endswith('.nii')])
+            rawdiffs = list(rootr for rootr,dirnamesr,filenamesr in os.walk(subject) if len(rootr.split('/'))==6 and 'DTI' in rootr.split('/')[3])
+            fadiffs = list(os.path.join(rootf,filenamef) for rootf,dirnamesf,filenamesf in os.walk(subject) if len(rootf.split('/'))==6 and 'FA' in rootf.split('/')[3] for filenamef in filenamesf if filenamef.endswith('.nii'))
+            for anat in anats:
+                run +=1
+                newfn = sub_name+'_run-0{}_T1w.nii'.format(run)
+                anatroot = '/'.join(anat.split('/')[:-1])
+                os.rename(anat, os.path.join(anatroot,newfn))
+                os.rename(os.path.join(anatroot, newfn), os.path.join(main_dir,sub_name,'anat',newfn))
+            run=0
+            for rawdiff in rawdiffs:
+                run+=1
+                newfn = sub_name+'_run-0{}_dwi'.format(run)
+                newdir = os.path.join(main_dir,sub_name,'dwi/')
+                cmd = "dcm2niix -b y -o {} -f {} {}".format(newdir,newfn,rawdiff)
+                process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+                output, error = process.communicate()
+                if not os.path.exists(srcdata+sub_name):
+                    os.mkdir(srcdata+sub_name)
+                os.rename(rawdiff,srcdata+sub_name+'/rawdcm_run-0{}/'.format(run))
+            run=0
+            for fadiff in fadiffs:
+                run+=1
+                if not os.path.exists(derivs+sub_name):
+                    os.mkdir(derivs+sub_name)
+                    os.mkdir(derivs+sub_name+'/fa/')
+                newfn = sub_name+'_run-0{}_dwi.nii'.format(run)
+                faroot = '/'.join(fadiff.split('/')[:-1])
+                os.rename(fadiff, os.path.join(faroot,newfn))
+                os.rename(os.path.join(faroot, newfn), os.path.join(derivs,sub_name,'fa',newfn))
+
+    print("... removing unneeded files ... \n")
+    while len(os.listdir(main_dir)) > sub+4:
+        for root, dirnames, filenames in os.walk(main_dir):
+            if os.path.isfile(os.path.join(root,'.DS_Store')):
+                os.remove(os.path.join(root,'.DS_Store'))
+            if os.path.isdir(root):
+                if not os.listdir(root):
+                    os.rmdir(root)
+    return
+
+if __name__=='__main__':
+
+    reformat(sys.argv[1])
--- neuro_format.py
+++ neuro_format.py
@@ -3,37 +3,51 @@ import sys
 from builtins import any as b_any
 
 """
-Combine multiple zip files into a single directory.
-Before running: navigate to external HD (in terminal) via
-    cd /Volumes/<name of HD>
-To run:
-    enter
-        python neuro_format.py <name_of_destination_directory> in terminal.
+FIRST FILE OF PIPELINE
+(Following multi-zip download from PPMI / other source into dir JustDownloaded/)
+
+Combines multiple zip files into a single directory.
+Before running: navigate to external HD (or location of files) in terminal via
+    `cd /Volumes/<name of HD>`
+To run (requires argument):
+    `python neuro_format.py <name_of_destination_directory>`
+
+Next file to run will be   `make_bids.py`
 """
-destination = './'+sys.argv[1]+'/' # path to destination folder
-if not os.path.exists(destination):
-    os.mkdir(destination)
 
-main_dir = "./JustDownloaded/" 
+def move_files(destdir,maindir):
+    print(f"Moving files from {maindir} and consolidating into {destdir}\n")
+    if not os.path.exists(destdir):
+        os.mkdir(destdir)
 
-fileroots = list([os.path.join(root,filename) for root,dirnames,filenames \
-                    in os.walk(main_dir) if len(root.split('/'))==7 \
-                    for filename in filenames])
-for filepath in fileroots:
-    filedest = list([rootd for rootd,dirnamesd,filenamesd \
-                    in os.walk(destination)])
-    for step in range(3,len(filepath.split('/')[:-1])):
-        if not b_any('/'.join(filepath.split('/')[3:step+1]).upper()==\
-                    '/'.join(dest.split('/')[2:step]).upper() \
-                    for dest in filedest if len(dest.split('/'))>=step):
-            os.mkdir(os.path.join(destination,\
-                    '/'.join(filepath.split('/')[3:step+1])))
-    os.rename(filepath, destination+'/'.join(filepath.split('/')[3:]))
+    fileroots = list([os.path.join(root,filename) for root,dirnames,filenames \
+                        in os.walk(maindir) if len(root.split('/'))==7 \
+                        for filename in filenames])
+    for filepath in fileroots:
+        filedest = list([rootd for rootd,dirnamesd,filenamesd \
+                        in os.walk(destination)])
+        for step in range(3,len(filepath.split('/')[:-1])):
+            if not b_any('/'.join(filepath.split('/')[3:step+1]).upper()==\
+                        '/'.join(dest.split('/')[2:step]).upper() \
+                        for dest in filedest if len(dest.split('/'))>=step):
+                os.mkdir(os.path.join(destination,\
+                        '/'.join(filepath.split('/')[3:step+1])))
+        os.rename(filepath, destination+'/'.join(filepath.split('/')[3:]))
+    print(f'... empyting {maindir} ...\n')
+    while len(os.listdir(maindir)) > 2:
+        for root, dirnames, filenames in os.walk(main_dir):
+            if os.path.isfile(os.path.join(root,'.DS_Store')):
+                os.remove(os.path.join(root,'.DS_Store'))
+            if os.path.isdir(root):
+                if not os.listdir(root):
+                    os.rmdir(root)
+    print(f"All files moved from {maindir} and consolidated into {destdir}\n\n")
+    return
 
-while len(os.listdir(main_dir)) > 2:
-    for root, dirnames, filenames in os.walk(main_dir):
-        if os.path.isfile(os.path.join(root,'.DS_Store')):
-            os.remove(os.path.join(root,'.DS_Store'))
-        if os.path.isdir(root):
-            if not os.listdir(root):
-                os.rmdir(root)
+if __name__=='__main__':
+
+    destination = './'+sys.argv[1]+'/' # path to destination folder
+
+    main_dir = "./JustDownloaded/"
+
+    move_files(destination,main_dir)
